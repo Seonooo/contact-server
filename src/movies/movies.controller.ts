54e70bcd570215802e1914d88addf73de8176378
GGwang2 <eedd22@naver.com>
2022-03-25 17:48:40 +0900

    0325

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,34 +1,33 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly moviesService: MoviesService) { }
+
     @Get()
-    getAll() {
-        return "This wii return all movies";
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
-    @Get("search")
-    search(@Query("year") searchingYear: string) {
-        return `We are searching for a movie made after: ${searchingYear}`;
-    }
-
-    @Get("/:id")
+    @Get(":id")
     getOne(@Param("id") movieId: string) {
-        return `This will return one movies with the id : ${movieId}`;
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData) {
-        return movieData;
+        return this.moviesService.create(movieData);
     }
-    @Delete("/:id")
+    @Delete(":id")
     remove(@Param('id') movieId: string) {
-        return `This will delete a movie${movieId}`;
+        return this.moviesService.deleteOne(movieId);
     }
 
     // put은 모든 리소스 업데이트, patch는 일부만 업데이트
-    @Patch('/:id')
+    @Patch(':id')
     path(@Param('id') movieId: string, @Body() movieUpdateData) {
         return {
             updateMovieId: movieId,
