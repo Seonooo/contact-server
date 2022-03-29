be312f9a21a0ec6a1abb3a6607bb86f122ed365a
GGwang2 <eedd22@naver.com>
2022-03-29 17:50:54 +0900

    0329

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -13,22 +14,22 @@ export class MoviesController {
     }
 
     @Get(":id")
-    getOne(@Param("id") movieId: string) {
+    getOne(@Param("id") movieId: number) {
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData) {
+    create(@Body() movieData: CreateMovieDto) {
         return this.moviesService.create(movieData);
     }
     @Delete(":id")
-    remove(@Param('id') movieId: string) {
+    remove(@Param('id') movieId: number) {
         return this.moviesService.deleteOne(movieId);
     }
 
     // put은 모든 리소스 업데이트, patch는 일부만 업데이트
     @Patch(':id')
-    path(@Param('id') movieId: string, @Body() movieUpdateData) {
+    path(@Param('id') movieId: number, @Body() movieUpdateData) {
         return this.moviesService.update(movieId, movieUpdateData);
     }
 
