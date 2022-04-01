4caae3e091278346f4602209e08b0e40cfcd9083
GGwang2 <eedd22@naver.com>
2022-04-01 17:50:38 +0900

    0401

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController], // url을 가져오고 함수를 실행시킴 router같은 존재
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // url을 가져오고 함수를 실행시킴 router같은 존재
+  providers: [],
 })
 export class AppModule { }
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
+
 
 @Controller('movies')
 export class MoviesController {
@@ -29,7 +31,7 @@ export class MoviesController {
 
     // put은 모든 리소스 업데이트, patch는 일부만 업데이트
     @Patch(':id')
-    path(@Param('id') movieId: number, @Body() movieUpdateData) {
+    path(@Param('id') movieId: number, @Body() movieUpdateData: UpdateMovieDto) {
         return this.moviesService.update(movieId, movieUpdateData);
     }
 
